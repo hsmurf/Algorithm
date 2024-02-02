822031b10b5805698e3dbddfee6e590568292857
황준용 <107796704+hsmurf@users.noreply.github.com>
2024-02-02 23:14:24 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.js
@@ -3,9 +3,8 @@ function solution(n) {
     let answer = [];
     let decimal = 0
     for (let remain = n; remain > 0; remain = Math.floor(remain / 3)){
-        answer.push(remain % 3)
+        answer.unshift(remain % 3)
     }
-    answer.reverse()
     for (let i = 0; i < answer.length; i++){
         decimal += (answer[i] * (3 ** i))  
     }
