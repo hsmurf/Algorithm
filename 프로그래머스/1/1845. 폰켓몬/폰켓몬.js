7bbebb9d9eb62df742b4fc1562530fd88d31218e
황준용 <107796704+hsmurf@users.noreply.github.com>
2024-06-02 03:22:40 +0900

    [level 1] Title: 폰켓몬, Time: 22.47 ms, Memory: 37.3 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.js
@@ -1,19 +1,13 @@
 function solution(nums) {
-    const keyLength = [...new Set(nums)].length
-    const halfLength = nums.length/2
-      
-    return keyLength < halfLength ? keyLength : halfLength
+    const maxPick = nums.length/2
+    const hash = {}
+    
+    for (let p of nums) {
+        hash[p] === undefined ? hash[p] = 1 : hash[p]++
+    }
+    
+    const ponketmonCount = Object.keys(hash).length;
+    
+    return ponketmonCount > maxPick ? maxPick : ponketmonCount
+ 
 }
-
-// 그냥 해시 reduce로 예전에 푼거 2.41ms/ 33.8mb 
-function solution(nums) {
-		const phone = nums.reduce((a,c)=>{
-        a[c]=(a[c]||0)+1
-        return a
-    },{})
-      
-      const keyLength = Object.keys(phone).length
-      const halfLength = nums.length/2
-      
-    return keyLength < halfLength ? keyLength : halfLength    
-  }
