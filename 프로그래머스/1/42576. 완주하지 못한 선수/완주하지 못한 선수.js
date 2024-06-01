c3296a8e8e71db2a477ebfef723319ae931b9c9b
황준용 <107796704+hsmurf@users.noreply.github.com>
2024-06-02 03:34:20 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 52.72 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,18 +1,18 @@
 function solution(participant, completion) {
     
-    const participantHash = participant.reduce((acc, cur) => {
+    const hash = participant.reduce((acc, cur) => {
       acc[cur] = acc[cur] ? ++acc[cur] : 1
         return acc
 }, {});
     
     completion.forEach((cur) => {
-        if (participantHash[cur]) {
-            participantHash[cur]--;
+        if (hash[cur]) {
+            hash[cur]--;
         }
     });
     
-    for (let key in participantHash) {
-        if (participantHash[key] === 1) {
+    for (let key in hash) {
+        if (hash[key] === 1) {
             return key;
         }
     }
